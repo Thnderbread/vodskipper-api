228f9addcd4db34ed86d968b3a1769fbae52a1b0
Thnderbread <david.kiiing1227@gmail.com>
2023-11-17 15:02:36 -0500

    Added memcached connection string environment variable.

--- src/config/MemClient.ts
+++ src/config/MemClient.ts
@@ -1,3 +1,5 @@
 import { Client } from "memjs"
 
-export const MemClient: Client<string | Buffer, Buffer> = Client.create()
+export const MemClient: Client<string | Buffer, Buffer> = Client.create(
+  process.env.AWS_MEMCACHED_NODE_EPT
+)
