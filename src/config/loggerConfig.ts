53efbaf674a268d482c2b114a41fd51c0e372833
Thnderbread <david.kiiing1227@gmail.com>
2023-11-17 14:59:08 -0500

    Refactored logger.
    - Reformatted the logger to have color input show for the entire text.
    - The logger used now depends on the environment variable LOG_OPTION. If it is set to "file", logs will be output to the files specified in the file. Otherwise, logs will be output to the console.

--- src/config/loggerConfig.ts
+++ src/config/loggerConfig.ts
@@ -1,5 +1,6 @@
 import path from "path"
 import { createLogger, format, transports } from "winston"
+import { addColors } from "winston/lib/winston/config"
 
 const logDir = path.join(__dirname, "..", "..", "logs")
 
@@ -7,7 +8,7 @@ const infoLogFilepath = path.join(logDir, "infoLog.log")
 const debugLogFilepath = path.join(logDir, "debugLog.log")
 const errorLogFilepath = path.join(logDir, "errorLog.log")
 
-export const fileLogger = createLogger({
+const fileLogger = createLogger({
   format: format.printf(({ message }) => {
     return `\n${message}\n${"-".repeat(160)}\n`
   }),
@@ -18,11 +19,42 @@ export const fileLogger = createLogger({
   ],
 })
 
-export const consoleLogger = createLogger({
+addColors({
+  error: "red",
+  info: "blue",
+  debug: "cyan",
+  warn: "yellow",
+})
+
+const alignLoggerOutput = format.combine(
+  format.combine(
+    format.label({
+      label: "[LOGGER]",
+    }),
+    format.timestamp({
+      format: "YY-MM-DD HH:mm:ss",
+    }),
+    format.colorize({
+      all: true,
+    }),
+    format.printf(
+      (info) =>
+        `${info.label}\t${info.timestamp}\t${info.level.toUpperCase()}\t${
+          info.message
+        }`
+    )
+  )
+)
+
+const consoleLogger = createLogger({
   format: format.json(),
   transports: [
     new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
+      format: format.combine(format.colorize(), alignLoggerOutput),
     }),
   ],
 })
+
+const logger = process.env.LOG_OPTION === "file" ? fileLogger : consoleLogger
+
+export default logger
