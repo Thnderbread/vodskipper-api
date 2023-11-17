0ec25a18520161895409e49e0c611f8830eeef45
Thnderbread <david.kiiing1227@gmail.com>
2023-11-17 15:33:10 -0500

    Interface for when Twurple Errors occur. Also defines a type guard fn.

---
+++ src/twurple/TwurpleError.ts
@@ -0,0 +1,15 @@
+export interface TwurpleError {
+  body: {
+    error: string
+    status: number
+    message: string
+  }
+}
+
+/**
+ * { body: string } symbolizes the JSON nature of
+ * the raw twurple response.
+ */
+export function isTwurpleError(error: unknown): error is { body: string } {
+  return typeof error === "object" && error !== null && "body" in error
+}
